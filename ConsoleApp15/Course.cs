2ff59467475de9fa08d5f730d1396496deef34d6
Rahuldal123 <rahuldalwale55@gmail.com>
2022-09-27 15:55:29 +0530

    Add project files.

---
+++ ConsoleApp15/Course.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+
+    public abstract class Displaydeatials
+    {
+        public abstract string details();
+    }
+    public  class Course:Displaydeatials
+    {
+        public string coursename;
+        public int fees;
+
+
+        public Course(string coursename, int fees)
+        {
+            this.coursename = coursename;
+            this.fees = fees;
+        }
+
+        public override string details()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"Course name ={coursename},fees={fees}";
+        }
+    }
+    public class Student : Displaydeatials
+    {
+        public int id;
+        public string name;
+        public string city;
+
+
+        public Student(int id, string name, string city)
+        {
+            this.id = id;
+            this.name = name;
+            this.city = city;
+        }
+
+        public override string details()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string displaydetails()
+        {
+            return $"student ID={id}, Name={name},City={city}";
+        }
+
+
+
+
+
+    }
+
+
+}
---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp15
+{
+    public class Student
+    {
+        public string Studname { get; set; }
+        public string City { get; set; }
+
+    }
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Fees { get; set; }
+        public Student[] Students { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] stud1 = new Student[]
+            {
+                new Student {Studname="Rahul",City="pune"},
+                new Student{Studname="Rohan",City="mumbai"}
+            };
+            Student[] stud2 = new Student[]
+            {
+                new Student{Studname="Mahesh",City="Kolhapur"},
+                new Student{Studname="akash",City="sangali"},
+                new Student{Studname="Rohit",City="Nagpur"}
+
+            };
+            Student[] stud3 = new Student[]
+            {
+                new Student {Studname="rakesh",City="Miraj"},
+                new Student{Studname="Omkar",City="jalgaon"},
+                new Student {Studname="ganesh",City="Amaravati"}
+
+            };
+            Course[]courses = new Course[]
+            {
+                new Course{Id =1,Name="c#",Fees=45000,Students=stud1},
+                new Course{Id =2,Name="java",Fees=55000,Students=stud2},
+                new Course{Id =3,Name="python",Fees=47000,Students=stud3}
+            };
+            foreach( Course c in courses)
+            {
+                Console.WriteLine($"{c.Id} {c.Name} {c.Fees}");
+                foreach(Student s in c. Students)
+                {
+                    Console.WriteLine($"\t{s.Studname}->{s.City}");
+                }
+            }
+        }
+    }
+}
